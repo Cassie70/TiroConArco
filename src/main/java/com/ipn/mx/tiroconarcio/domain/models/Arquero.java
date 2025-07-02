6b4d12b078c9d475ca901277ac3f23690f7fadbe
cassie70 <yael07mora@gmail.com>
2025-07-02 10:17:48 -0600

    ci jijij

--- src/main/java/com/ipn/mx/tiroconarcio/domain/models/Arquero.java
+++ src/main/java/com/ipn/mx/tiroconarcio/domain/models/Arquero.java
@@ -1,6 +1,7 @@
 package com.ipn.mx.tiroconarcio.domain.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -46,9 +47,11 @@ public class Arquero implements Serializable {
 
     @Lob
     @Column(name = "image", columnDefinition = "BYTEA")
+    @JsonIgnore
     private byte[] image;
 
     @Column(name = "image_type")
+    @JsonIgnore
     private String imageType;
 
 }
